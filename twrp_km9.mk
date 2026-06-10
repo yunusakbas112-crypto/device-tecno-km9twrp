7d5e329cbaf02d1ceb41f930a8daa384a4a354ff
yunusakbas112-crypto <yunusakbas112@gmail.com>
2026-06-10 17:23:42 +0300

    Update and rename omni_km9.mk to twrp_km9.mk

--- twrp_km9.mk
+++ twrp_km9.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := omni_km9
+PRODUCT_NAME := twrp_km9
 PRODUCT_DEVICE := km9
 PRODUCT_BRAND := Tecno
 PRODUCT_MANUFACTURER := Tecno
