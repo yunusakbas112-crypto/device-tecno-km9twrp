cf3e0b375158b5e1f5e5790a61011ef2c6368283
yunusakbas112-crypto <yunusakbas112@gmail.com>
2026-06-10 17:35:48 +0300

    Update twrp_km9.mk

--- twrp_km9.mk
+++ twrp_km9.mk
@@ -1,6 +1,10 @@
+# Orijinal Android 64-bit kütüphanelerini dahil et
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
 
+# TWRP'nin ana kütüphanelerini çağır (Hata veren omni satırı yerine bu gelir)
+$(call inherit-product, bootable/recovery/config/twrp_product.mk)
+
+# Pazarlama ve Cihaz Tanımlamaları
 PRODUCT_NAME := twrp_km9
 PRODUCT_DEVICE := km9
 PRODUCT_BRAND := Tecno
